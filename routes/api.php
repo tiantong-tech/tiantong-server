a54acbc8c9d7af498d2dc61704bbe263b517b9ba
delylaric <delylaric@gmail.com>
2019-06-02 23:44:19 +0800

    替换根路由

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+// 	return $request->user();
+// });
+
+Route::get('/', function () {
+	return [
+		'message' => '天瞳系统 v1.0'
+	];
 });
