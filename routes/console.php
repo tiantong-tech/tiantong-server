ecd9be3fbe8ca71c4b5bf1f78afc2fe80f4fc599
delylaric <delylaric@gmail.com>
2019-06-03 13:00:16 +0800

    新增'db:rebuild'命令重建数据库及数据

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,8 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('db:rebuild', function () {
+	$this->call('migrate:refresh');
+	$this->call('db:seed');
+})->describe('migrate:refresh && db:seed');
