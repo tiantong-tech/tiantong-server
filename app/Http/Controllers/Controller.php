35903eed3d3d8e401f561c3501ff6e6eab2dad9d
delylaric <delylaric@gmail.com>
2019-06-04 16:02:55 +0800

    完善BaseController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,67 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+	public function viaParams($params, $rules)
+	{
+		$validator = \Validator::make($params, $rules);
+
+		if ($validator->fails()) {
+			throw new \Illuminate\Validation\ValidationException($validator);
+		}
+
+		$result = [];
+
+		foreach ($rules as $key => $value) {
+			if (isset($params[$key])) {
+				$result[$key] = $params[$key];
+			} else {
+				$result[$key] = null;
+			}
+		}
+
+		return $result;
+	}
+
+	public function via(array $rules)
+	{
+		$params = request(array_keys($rules));
+
+		return $this->viaParams($params, $rules);
+	}
+
+	public function all()
+	{
+		return request()->all();
+	}
+
+	public function get($name, $rule = 'nullable', $default = null)
+	{
+		$params = request([$name]);
+		$this->viaParams($params, [$name => $rule]);
+		$param = isset($params[$name]) ? $params[$name] : $default;
+
+		return $param;
+	}
+
+	public function success($messages, $status = 201)
+	{
+		if (!is_array($messages)) {
+			$messages = ['message' => $messages];
+		}
+
+		return response()->json($messages, $status);
+	}
+
+	public function failure($messages, $status = 400)
+	{
+		if (!is_array($messages)) {
+			$messages = ['message' => $messages];
+		}
+
+		throw new \App\Exceptions\CustomerExpection(
+			response()->json($messages, $status)
+    );
+	}
 }
