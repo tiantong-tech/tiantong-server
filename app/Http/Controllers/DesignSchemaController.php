31a02b2348b38084e96296e839884403a95aed75
zhanglan <zhanglan1315@gmail.com>
2019-08-18 15:40:57 +0800

    feat(projects): DesignSchema的业务逻辑

---
+++ app/Http/Controllers/DesignSchemaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Transaction;
+use App\Services\Enums;
+use App\Models\Project;
+use App\Models\Quotation;
+use App\Models\CadDrawing;
+use App\Models\DesignSchema;
+use Illuminate\Validation\Rule;
+
+class DesignSchemaController extends Controller
+{
+  use Traits\ProjectTrait;
+
+  public function search()
+  {
+    return DesignSchema::all();
+  }
+
+  public function create()
+  {
+    $type = $this->get('type', Rule::in(Enums::designSchemaTypes), 'hoister');
+    $data = $this->getInputData('');
+    $project = $this->getProject();
+    $schema = new DesignSchema;
+    $schema->project_id = $project->id;
+    $schema->fill($data);
+    $schema->type = $type;
+
+    Transaction::begin();
+    $schema->save();
+    Project::where('id', $project->id)->update([
+      'design_schema_ids' => DB::raw("design_schema_ids || $schema->id")
+    ]);
+    Transaction::commit();
+
+    return $this->success('success to add design schema');
+  }
+
+  public function update()
+  {
+    $project = $this->getProject();
+    $schema = $this->getDesignSchema();
+
+    $data = $this->via([
+      'data' => 'json',
+      'name' => 'string',
+      'notes' => 'string',
+      'company' => 'string',
+      'customer_information' => 'string',
+    ]);
+
+    $schema->fill($data);
+    $schema->save();
+
+    return $this->success('success to update project information');
+  }
+
+  public function delete()
+  {
+    $schema = $this->getDesignSchema();
+
+    Transaction::begin();
+    $schema->delete();
+    Quotation::where('design_schema_id', $schema->id)->delete();
+    CadDrawing::where('design_schema_id', $schema->id)->delete();
+    Project::where('id', $schema->project_id)->update([
+      'design_schema_ids' => DB::raw("array_remove(design_schema_ids, $schema->id)")
+    ]);
+    Transaction::commit();
+
+    return $this->success('success to delete project');
+  }
+
+  private function getInputData($default = false)
+  {
+    return $this->via([
+      'quantity' => 'string',
+      'name' => 'string',
+      'data' => 'json'
+    ], $default);
+  }
+}
