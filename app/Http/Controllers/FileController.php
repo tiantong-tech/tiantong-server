110c21c12ae9167abaa6ad3fbf72e1d925832917
zhanglan <zhanglan1315@gmail.com>
2019-08-30 17:21:53 +0800

    feat(projects): 文件重命名

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -7,45 +7,22 @@ use Qiniu;
 use Carbon\Carbon;
 use App\Models\File;
 use App\Exceptions\HttpException;
+use App\Http\Controllers\Traits;
 
 class FileController extends Controller
 {
+  use Traits\FileTrait;
+
   public function update()
   {
     $data = $this->via([
-      'name' => 'string'
+      'name' => 'string',
+      'comment' => 'string',
     ]);
     $file = $this->getFile();
     $file->fill($data);
     $file->save();
 
     return $this->success('success_to_update_file_data');
-  }
-
-  public function confirmUpload()
-  {
-    $link = $this->get('link', 'required');
-    $file = $this->getFile();
-    $file->link = $link;
-    $file->status = '已上传';
-    $file->save();
-
-    return $this->success('success_to_confirm_file_upload');
-  }
-
-  protected function getFile()
-  {
-    $id = $this->get('id');
-    $userId = Auth::user()->id;
-    $file = File::find($id);
-    if (!$file) {
-      throw new HttpException('fail to find file by id', 404);
-    }
-
-    if ($file->user_id !== $userId) {
-      throw new HttpException('fail to authenticate file by user id', 401);
-    }
-
-    return $file;
   }
 }
